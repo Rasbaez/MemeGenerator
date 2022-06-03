d88c6c2c85aa9092eec738fd8f9f91456c799e29
Roberto Baez <rbaezsp@gmail.com>
2022-06-03 15:06:00 -0300

    req 6

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const userText = document.querySelector('#text-input');
 const text = document.querySelector('#meme-text');
 const memeContainer = document.querySelector('#meme-image-container');
 const imgMeme = document.getElementById('meme-insert');
+const btnFire = document.querySelector('#fire');
+const btnWater = document.querySelector('#water');
+const btnEarth = document.querySelector('#earth');
+const solidBorder = '1px solid black';
 
 function showText() {
   text.innerText = userText.value;
@@ -23,3 +27,29 @@ function readImage() {
 
 imgMeme.addEventListener('change', readImage);
 
+function fireBorder(event) {
+  const fire = '3px dashed rgb(255, 0, 0)';
+  memeContainer.style.border = fire
+  // if (memeContainer.style.border === solidBorder) {
+   
+  
+}
+
+function whaterBorder() {
+  const water = '5px double rgb(0, 0, 255)';
+  memeContainer.style.border = water;
+  // if (memeContainer.style.border === solidBorder) {
+  //   return 
+  // // } else {
+  // //   return memeContainer.style.border = solidBorder;
+  // // }
+}
+
+function earthBorder() {
+  const earth = '6px groove rgb(0, 128, 0)';
+  
+   memeContainer.style.border = earth;
+  // } else {
+  //   return memeContainer.style.border = solidBorder;
+  // }
+}
